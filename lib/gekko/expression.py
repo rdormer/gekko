baf5fe4e4df3fe1cdf20edb16d93c638d779a3cd
Robert Dormer <rdormer@gmail.com>
2025-01-25 16:23:53 -0500

    Internals for grouping functions, starting with count and as_var

--- lib/gekko/expression.py
+++ lib/gekko/expression.py
@@ -2,14 +2,18 @@ class Expression:
     def __init__(self, expression):
         self.expression = expression
         setattr(self, 'round', self.__round)
+        setattr(self, 'count', self.__count)
+        setattr(self, 'as_var', self.__as_var)
         setattr(self, 'add_column', self.__add_column)
         setattr(self, 'as_percent', self.__as_percent)
+        self.memoize_as = self.expression
 
-    def eval(self, symbols):
+    def eval(self, symbols, memo=None):
         for var in symbols:
             obj = self.__cast_type(symbols[var])
             setattr(self, var, obj)
 
+        self.memo = memo
         self.symbols = symbols
         return eval(self.expression, self.__dict__)
 
@@ -32,3 +36,18 @@ class Expression:
 
     def __round(self, value, places=2):
         return str(round(value, places))
+
+    def __count(self, filter=True):
+        self.memo.setdefault(self.memoize_as, 0)
+
+        if filter:
+            self.memo[self.memoize_as] += 1
+
+    def __as_var(self, varname, value):
+        srckey = self.memoize_as
+        if self.memoize_as != varname:
+            self.memo[varname] = self.memo[srckey]
+            self.memoize_as = varname
+            del self.memo[srckey]
+
+        setattr(self, varname, self.memo[varname])
--- lib/gekko/tables.py
+++ lib/gekko/tables.py
@@ -1,6 +1,5 @@
 from lib.gekko.expression import Expression
 
-
 class Table:
     NO_GROUP_KEY = '--none--'
 
@@ -10,20 +9,22 @@ class Table:
         self.headers = None
         self.config = config
         self.groups = {}
+        self.data = {}
 
-    def each_group(self, fn, data=None):
-        data = data or self.groups
+    def each_group(self, fn, data=None, path=None):
+        data = data or self.data
+        path = path or []
 
         if type(data) == list:
-            fn(data)
+            fn(data, path)
         else:
             for group in data:
-                self.each_group(fn, data[group])
+                self.each_group(fn, data[group], path + [group])
 
     def each_row(self, row_fn, data=None):
-        data = data or self.groups
+        data = data or self.data
 
-        def group_fn(data):
+        def group_fn(data, path):
             nonlocal row_fn
             for row in data:
                 row_fn(row)
@@ -45,12 +46,15 @@ class Table:
 
     def __evaluate(self):
         if not 'group' in self.config:
-            self.groups[self.NO_GROUP_KEY] = []
+            self.data[self.NO_GROUP_KEY] = []
 
         self.__load_data()
 
         if 'per_row' in self.config:
             self.each_row(self.__row_evaluate)
+
+        if 'per_group' in self.config:
+            self.each_group(self.__group_evaluate)
 
     def __row_evaluate(self, row):
         header_vars = self.headers.row_map(row)
@@ -59,6 +63,17 @@ class Table:
             for newcol in changes:
                 self.headers.add_column(newcol)
                 row.append(changes[newcol])
+
+    def __group_evaluate(self, group, path):
+        group_memo = {}
+        expressions = [ Expression(expr) for expr in self.config['per_group'] ]
+
+        for row in group:
+            row_map = self.headers.row_map(row)
+            for expr in expressions:
+                expr.eval(row_map, group_memo)
+
+        self.__upsert(self.groups, path, lambda val: group_memo)
 
     def __load_data(self):
         if 'table' in self.config:
@@ -79,8 +94,8 @@ class Table:
                     data.each_row(self.__append_row)
 
     def __append(self, srctable):
-        for group in srctable.groups:
-            for row in srctable.groups[group]:
+        for group in srctable.data:
+            for row in srctable.data[group]:
                 if 'group' in self.config:
                     self.__collect_groups(row, [], -1)
                 else:
@@ -95,28 +110,25 @@ class Table:
     # creates or appends to an array of the rows for that group
 
     def __collect_groups(self, row, headers, idx):
-        group_keys = self.config['group']
-        current_tree = self.groups
-
-        for group in group_keys[:-1]:
-            group_index = headers.index(group)
-            group_value = row[group_index]
-
-            if not group_value in current_tree:
-                current_tree[group_value] = {}
-
-            current_tree = current_tree[group_value]
-
-        group_index = headers.index(group_keys[-1])
-        last_group = row[group_index]
-
-        if last_group in current_tree:
-            current_tree[last_group] += [row]
-        else:
-            current_tree[last_group] = [row]
+        header_idxs = [headers.index(group) for group in self.config['group']]
+        path = [row[idx] for idx in header_idxs]
+        appendrow = lambda value: [row] if value == None else value + [row]
+        self.__upsert(self.data, path, appendrow)
 
     # Default method for when no grouping is defined.  Just throw each row
     # into a single internal key used to indicate that no explicit group exists
 
     def __append_row(self, row, headers, idx):
-        self.groups[self.NO_GROUP_KEY].append(row)
+        self.data[self.NO_GROUP_KEY].append(row)
+
+    def __upsert(self, target, path, val_fn):
+        current = target
+        for key in path[:-1]:
+            if key in target:
+                current = target[key]
+            else:
+                current = {}
+                target[key] = current
+
+        key = path[-1:][0]
+        target[key] = val_fn(target.get(key))
