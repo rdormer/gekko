77a46ca0cbfb87c31a7fe9a54be08a47edc931d4
Robert Dormer <rdormer@gmail.com>
2025-05-22 11:23:50 -0400

    Log all failures in test runner instead of bombing on the first failure

--- test/suite.py
+++ test/suite.py
@@ -1,10 +1,15 @@
 import glob
 import os
 
+ANSI_RED = '\033[91m'
+ANSI_RESET = '\033[0m'
+
 files = glob.glob('test/reports/**/*.yml', recursive=True)
 for report in files:
     print(report)
     testout = os.popen("./gekko " + report).read()
     trueval = report.replace('reports', 'outputs').replace('.yml', '.txt')
     correctout = os.popen("cat " + trueval).read()
-    assert(testout == correctout)
+
+    if(testout != correctout):
+        print(ANSI_RED + "FAIL: " + report + ANSI_RESET)
